f5c0e0bf7161c619a0f2f4841ff74616f72a7ebd
Martin Marinov <martin.marinov91@gmail.com>
2013-06-09 01:15:57 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,50 @@
+import socket
+import datetime
+import os
+import timeit
+import threading
+import time
+
+errors = 0
+
+def make_requests(times):
+    for i in xrange(times):
+        # message = raw_input("> ")
+        message = "user blabla%d" % i
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            HOST, PORT = ('127.0.0.1', 53617)
+            # Connect to server and send data
+            # start_time = datetime.datetime.now()
+            s.connect((HOST, PORT))
+            s.sendall(message + "\n")
+
+            # Receive data from the server and shut down
+            received = s.recv(1024)
+            # end_time = datetime.datetime.now()
+            # print "Received : %s" % received
+            # microseconds = (end_time - start_time).microseconds
+            # print "Thread: %s ; RequestNo: %d ;Time %s" % (threading.currentThread().getName(), i, repr(microseconds))
+        except:
+            global errors
+            errors += 1
+        finally:
+            s.close()
+
+if __name__ == '__main__':
+    start_time = datetime.datetime.now()
+    for client in xrange(1, 500):
+        t = threading.Thread(target=make_requests, args=(100, ))
+        # t.setDaemon(True) # don't hang on exit
+        t.setName("Thread%d" % client)
+        t.start()
+        t.join()
+    end_time = datetime.datetime.now()
+    print "Total time: %d" % (end_time-start_time).seconds
+        # pid = os.fork()
+        # if pid:
+        #     continue
+        # else:
+        #     print "PID %d: %f" % (os.getpid(), timeit.timeit('make_requests(30)', setup='from __main__ import make_requests', number=1))
+        #     os._exit(0)
+
---
+++ server.py
@@ -0,0 +1,48 @@
+import SocketServer
+from mixins import ThreadPoolMixIn
+import re
+import threading
+import os
+
+
+class MyRequestHandler(SocketServer.StreamRequestHandler):
+    users = {}
+
+    def handle(self):
+        # print threading.currentThread().getName()
+        # print "Pid: %d" % os.getpid()
+        command = self.rfile.readline().strip()
+        user = re.search('user (\w+)', command)
+        response = None
+        if user:
+            user = user.group(1)
+            if user and user in self.users:
+                response = "100 err %s already taken!" % user
+            else:
+                self.users[user] = self.client_address
+                response = "200 ok %s successfully registerred" % user
+        #import pdb;pdb.set_trace()
+        self.wfile.write(response)
+        return
+
+
+class MyThreadedServer(ThreadPoolMixIn, SocketServer.TCPServer):
+    numThreads = 300
+
+class MyForkedServer(SocketServer.ForkingTCPServer):
+    max_children = 40
+
+
+# class COMMANDS:
+
+
+
+# class CommandParser:
+#     def __init__(self, request):
+#         command = request.recv(1024).decode('utf8')
+
+if __name__ == '__main__':
+    address = ('localhost', 53617)  # let the kernel give us a port
+    server = MyThreadedServer(address, MyRequestHandler)
+
+    server.serve_forever()
